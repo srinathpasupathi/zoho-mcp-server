f9a93d552f0b7b3a0193688ebbe7efda45291f37
David Cramer <dcramer@gmail.com>
2025-04-02 12:01:14 -0700

    Add copy paste helper

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,34 @@
 import { Hono } from "hono";
 import SentryMCP from "../mcp";
 
+const copyPasteHelper = `
+const nodes = document.querySelectorAll("[data-copy]");
+nodes.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const text = button.getAttribute("data-copy");
+    navigator.clipboard.writeText(decodeURIComponent(text));
+  });
+});
+`;
+
 export default new Hono<{
   Bindings: Env & {
     MCP_OBJECT: DurableObjectNamespace<SentryMCP>;
   };
 }>().get("/", async (c) => {
+  const mcpSnippet = JSON.stringify(
+    {
+      mcpServers: {
+        sentry: {
+          command: "npx",
+          args: ["mcp-remote", new URL("/sse", c.req.url).href],
+        },
+      },
+    },
+    undefined,
+    2
+  );
+
   return c.html(
     <html>
       <head>
@@ -17,25 +40,19 @@ export default new Hono<{
           This service provides a Model Context Provider for interacting with{" "}
           <a href="https://docs.sentry.io/api/">Sentry's API</a>.
         </p>
-        <pre>
-          {JSON.stringify(
-            {
-              mcpServers: {
-                sentry: {
-                  command: "npx",
-                  args: ["mcp-remote", new URL("/sse", c.req.url).href],
-                },
-              },
-            },
-            undefined,
-            2
-          )}
-        </pre>
+        <div>
+          <button type="button" data-copy={mcpSnippet}>
+            Copy
+          </button>
+          <pre>{mcpSnippet}</pre>
+        </div>
         <h2>Available Tools</h2>
         <p>TODO:</p>
         <footer>
           <a href="https://github.com/getsentry/sentry-mcp">GitHub</a>
         </footer>
+
+        <script dangerouslySetInnerHTML={{ __html: copyPasteHelper }} />
       </body>
     </html>
   );
