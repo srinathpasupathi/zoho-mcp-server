345748b8332131b9edaf9a5d162527b56abdbb47
David Cramer <dcramer@gmail.com>
2025-04-03 13:31:57 -0400

    Styles

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import type SentryMCP from "../mcp";
+import { css, Style } from "hono/css";
 
 const copyPasteHelper = `
 const nodes = document.querySelectorAll("[data-copy]");
@@ -9,6 +10,114 @@ nodes.forEach((button) => {
     navigator.clipboard.writeText(decodeURIComponent(text));
   });
 });
+`;
+
+const globalStyles = css`
+  :root {
+    --font-sans: ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji",
+      "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    --font-mono: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+      "Liberation Mono", "Courier New", monospace;
+
+    --default-font-family: var(--font-sans);
+    --default-mono-font-family: var(--font-mono);
+
+    --tw-prose-body: oklch(37.3% 0.034 259.733);
+    --tw-prose-headings: oklch(21% 0.034 264.665);
+    --tw-prose-links: oklch(21% 0.034 264.665);
+    --tw-prose-bold: oklch(21% 0.034 264.665);
+    --tw-prose-code: oklch(21% 0.034 264.665);
+    --tw-prose-pre-code: oklch(92.8% 0.006 264.531);
+    --tw-prose-pre-bg: oklch(27.8% 0.033 256.848);
+
+    --color-white: #fff;
+    --color-gray-500: oklch(55.1% 0.027 264.364);
+    --color-gray-600: oklch(44.6% 0.03 256.802);
+  }
+
+  html {
+    font-family: var(
+      --default-font-family,
+      ui-sans-serif,
+      system-ui,
+      sans-serif,
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "Noto Color Emoji"
+    );
+
+    line-height: 1.5;
+    -webkit-text-size-adjust: 100%;
+    tab-size: 4;
+  }
+
+  body {
+    padding: 2rem;
+    background: #fff;
+    color: var(--tw-prose-body);
+    font-size: 1rem;
+    line-height: 1.75;
+  }
+
+  h1 {
+    color: var(--tw-prose-headings);
+    font-weight: 800;
+    font-size: 2.25em;
+    margin-top: 0;
+    margin-bottom: 0.8888889em;
+    line-height: 1.1111111;
+  }
+
+  p,
+  h1,
+  h2,
+  pre {
+    margin: 1.25rem 0;
+  }
+
+  a {
+    color: var(--tw-prose-links);
+    text-decoration: underline;
+    font-weight: 500;
+  }
+
+  pre {
+    font-family: var(
+      --default-mono-font-family,
+      ui-monospace,
+      SFMono-Regular,
+      Menlo,
+      Monaco,
+      Consolas,
+      "Liberation Mono",
+      "Courier New",
+      monospace
+    );
+    color: var(--tw-prose-pre-code);
+    background-color: var(--tw-prose-pre-bg);
+    overflow-x: auto;
+    font-weight: 400;
+    font-size: 0.875em;
+    border-radius: 0.375rem;
+    padding: 0.85em 1.15em;
+  }
+
+  button {
+    border-radius: 0.25rem;
+    border: 0;
+
+    color: var(--color-white);
+    background-color: var(--color-gray-600);
+    padding: 0.25rem 0.5rem;
+    font-size: 0.75rem;
+    font-weight: 600;
+    cursor: pointer;
+
+    &:hover {
+      background-color: var(--color-gray-500);
+    }
+  }
 `;
 
 export default new Hono<{
@@ -30,9 +139,10 @@ export default new Hono<{
   );
 
   return c.html(
-    <html>
+    <html lang="en">
       <head>
         <title>Sentry MCP</title>
+        <Style>{globalStyles}</Style>
       </head>
       <body>
         <h1>Sentry MCP</h1>
@@ -42,7 +152,7 @@ export default new Hono<{
         </p>
         <div>
           <button type="button" data-copy={mcpSnippet}>
-            Copy
+            Copy Configuration
           </button>
           <pre>{mcpSnippet}</pre>
         </div>
