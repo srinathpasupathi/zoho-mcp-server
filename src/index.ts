757e3f49bdc0426606d56b8da9f7a5be700d621a
David Cramer <dcramer@gmail.com>
2025-04-03 13:01:05 -0400

    Remove abstraction

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import { withSentry } from "@sentry/cloudflare";
-import oAuthProvider from "./oauth-provider";
+import OAuthProvider from "@cloudflare/workers-oauth-provider";
+import app from "./app";
+import SentryMCP from "./mcp";
 
 // required for Durable Objects
 export { default as SentryMCP } from "./mcp";
+
+const oAuthProvider = new OAuthProvider({
+  apiRoute: "/sse",
+  // @ts-ignore
+  apiHandler: SentryMCP.mount("/sse"),
+  // @ts-ignore
+  defaultHandler: app,
+  authorizeEndpoint: "/authorize",
+  tokenEndpoint: "/token",
+  clientRegistrationEndpoint: "/register",
+});
 
 export default withSentry(
   (env) => ({
