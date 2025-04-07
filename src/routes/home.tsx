a7f482a9817d90aa79471bc8125f1ee81167ba47
David Cramer <dcramer@gmail.com>
2025-04-06 19:02:31 -0700

    Add junk ToC

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -52,6 +52,37 @@ const globalStyles = css`
     line-height: 1.75;
   }
 
+
+  main {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    max-width: 1000px;
+
+    article {
+      flex: 1;
+    }
+
+    /** TODO: Make this sticky */
+    nav {
+      width: 200px;
+
+      li ul {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 800px) {
+    article {
+      margin-right: 0;
+    }
+
+    nav {
+      display: none;
+    }
+  }
+
   h1 {
     color: var(--tw-prose-headings);
     font-weight: 800;
@@ -69,7 +100,7 @@ const globalStyles = css`
   ol,
   pre,
   blockquote {
-    margin: 1.25rem 0;
+    margin: 0 0 1.25rem;
   }
 
   a {
@@ -102,12 +133,18 @@ const globalStyles = css`
     padding: 0.85em 1.15em;
   }
 
-  small {
-    font-size: 0.85rem;
+  .snippet {
+    position: relative;
+
+    button {
+      position: absolute;
+      right: 0.25rem;
+      top: 0.25rem;
+    }
   }
 
-  article {
-    max-width: 600px;
+  small {
+    font-size: 0.85rem;
   }
 
   button {
@@ -168,154 +205,206 @@ export default new Hono<{
         <Style>{globalStyles}</Style>
       </head>
       <body>
-        <article>
-          <h1>Sentry MCP</h1>
-          <p>
-            This service provides a Model Context Provider for interacting with{" "}
-            <a href="https://docs.sentry.io/api/">Sentry's API</a>.
-          </p>
-          <p>
-            While this service is maintained by Sentry, it is very much still a
-            proof-of-concept as the protocol is still in development (as is our
-            own thinking around its usage).
-          </p>
-          <div>
-            <button type="button" data-copy={mcpSnippet}>
-              Copy Configuration
-            </button>
-            <pre>{mcpSnippet}</pre>
-          </div>
-          <h3>With Cursor</h3>
-          <ol>
-            <li>
-              <strong>Cmd + Shift + J</strong> to open Cursor Settings.
-            </li>
-            <li>
-              Select <strong>MCP</strong>.
-            </li>
-            <li>
-              Select <strong>Add new global MCP server</strong>.
-            </li>
-          </ol>
-          <h3>With Windsurf</h3>
-          <ol>
-            <li>Open Windsurf Settings.</li>
-            <li>
-              Under <strong>Cascade</strong>, you'll find{" "}
-              <strong>Model Context Provider Servers</strong>.
-            </li>
-            <li>
-              Select <strong>Add Server</strong>.
-            </li>
-          </ol>
-          <p>
-            <small>
-              Note: Windsurf requires an enterprise account to utilize MCP. 😕
-            </small>
-          </p>
+        <div class="container">
+          <header>
+            <h1>Sentry MCP</h1>
+          </header>
+          <main>
+            <article>
+              <div id="top" />
+              <p>
+                This service provides a Model Context Provider for interacting
+                with <a href="https://docs.sentry.io/api/">Sentry's API</a>.
+              </p>
+              <p>
+                While this service is maintained by Sentry, it is very much
+                still a proof-of-concept as the protocol is still in development
+                (as is our own thinking around its usage).
+              </p>
+              <div class="snippet">
+                <button type="button" data-copy={mcpSnippet}>
+                  Copy Configuration
+                </button>
+                <pre>{mcpSnippet}</pre>
+              </div>
 
-          <h3>With VSCode</h3>
-          <ol>
-            <li>CMD+P</li>
-            <li>
-              Select <strong>MCP: Add Server...</strong>
-            </li>
-            <li>
-              Select <strong>Command (stdio)</strong>.
-            </li>
-            <li>
-              Enter <code>npx mcp-remote https://sentry.cool/sse</code>{" "}
-              <button
-                type="button"
-                data-copy="npx mcp-remote https://sentry.cool/sse"
-              >
-                Copy
-              </button>
-            </li>
-            <li>
-              Enter <code>Sentry</code>
-            </li>
-            <li>
-              Select <strong>User settings</strong> or{" "}
-              <strong>Workspace settings</strong> (to limit to specific project)
-            </li>
-          </ol>
-          <p>
-            <small>Note: MCP is supported in VSCode 1.99 and above.</small>
-          </p>
+              <section className="setup-guide">
+                <h3 id="with-cursor">With Cursor</h3>
+                <ol>
+                  <li>
+                    <strong>Cmd + Shift + J</strong> to open Cursor Settings.
+                  </li>
+                  <li>
+                    Select <strong>MCP</strong>.
+                  </li>
+                  <li>
+                    Select <strong>Add new global MCP server</strong>.
+                  </li>
+                </ol>
+              </section>
 
-          <h2>Workflows</h2>
-          <section className="workflows">
-            <p>
-              Here's a few sample workflows (prompts) that we've tried to design
-              around within the provider:
-            </p>
-            <ul>
-              <li>
-                <blockquote>
-                  Check Sentry for errors in <code>@file.tsx</code> and propose
-                  solutions.
-                </blockquote>
-              </li>
-              <li>
-                <blockquote>
-                  Diagnose issue <code>ISSUE_URL</code> in Sentry and propose
-                  solutions.
-                </blockquote>
-              </li>
-              <li>
-                <blockquote>
-                  Create a new project in Sentry for <code>service-name</code>{" "}
-                  and setup local instrumentation using it.
-                </blockquote>
-              </li>
-            </ul>
-          </section>
+              <section className="setup-guide">
+                <h3 id="with-windsurf">With Windsurf</h3>
+                <ol>
+                  <li>Open Windsurf Settings.</li>
+                  <li>
+                    Under <strong>Cascade</strong>, you'll find{" "}
+                    <strong>Model Context Provider Servers</strong>.
+                  </li>
+                  <li>
+                    Select <strong>Add Server</strong>.
+                  </li>
+                </ol>
+                <p>
+                  <small>
+                    Note: Windsurf requires an enterprise account to utilize
+                    MCP. 😕
+                  </small>
+                </p>
+              </section>
 
-          <h2>Available Tools</h2>
-          <section className="tools">
-            <p>
-              <small>
-                Note: Any tool that takes an <code>organization_slug</code>{" "}
-                parameter will try to infer a default organization, otherwise
-                you should mention it in the prompt.
-              </small>
-            </p>
-            <ul>
-              {TOOL_DEFINITIONS.map((tool) => (
-                <li key={tool.name}>
-                  <h3>{tool.name}</h3>
+              <section className="setup-guide">
+                <h3 id="with-vscode">With VSCode</h3>
+                <ol>
+                  <li>CMD+P</li>
+                  <li>
+                    Select <strong>MCP: Add Server...</strong>
+                  </li>
+                  <li>
+                    Select <strong>Command (stdio)</strong>.
+                  </li>
+                  <li>
+                    Enter <code>npx mcp-remote https://sentry.cool/sse</code>{" "}
+                    <button
+                      type="button"
+                      data-copy="npx mcp-remote https://sentry.cool/sse"
+                    >
+                      Copy
+                    </button>
+                  </li>
+                  <li>
+                    Enter <code>Sentry</code>
+                  </li>
+                  <li>
+                    Select <strong>User settings</strong> or{" "}
+                    <strong>Workspace settings</strong> (to limit to specific
+                    project)
+                  </li>
+                </ol>
+                <p>
+                  <small>
+                    Note: MCP is supported in VSCode 1.99 and above.
+                  </small>
+                </p>
+              </section>
 
-                  {tool.paramsSchema ? (
-                    <dl class="params">
-                      {Object.entries(tool.paramsSchema).map(([key, value]) => {
-                        return (
-                          <>
-                            <dt key={key}>
-                              <code>{key}</code>
-                            </dt>
-                            <dd key={key}>{value.description}</dd>
-                          </>
-                        );
-                      })}
-                    </dl>
-                  ) : null}
+              <section className="workflows" id="workflows">
+                <h2>Workflows</h2>
+                <p>
+                  Here's a few sample workflows (prompts) that we've tried to
+                  design around within the provider:
+                </p>
+                <ul>
+                  <li>
+                    <blockquote>
+                      Check Sentry for errors in <code>@file.tsx</code> and
+                      propose solutions.
+                    </blockquote>
+                  </li>
+                  <li>
+                    <blockquote>
+                      Diagnose issue <code>ISSUE_URL</code> in Sentry and
+                      propose solutions.
+                    </blockquote>
+                  </li>
+                  <li>
+                    <blockquote>
+                      Create a new project in Sentry for{" "}
+                      <code>service-name</code> and setup local instrumentation
+                      using it.
+                    </blockquote>
+                  </li>
+                </ul>
+              </section>
 
-                  <p>{tool.description.split("\n")[0]}</p>
+              <section className="tools" id="tools">
+                <h2>Available Tools</h2>
+                <p>
+                  <small>
+                    Note: Any tool that takes an <code>organization_slug</code>{" "}
+                    parameter will try to infer a default organization,
+                    otherwise you should mention it in the prompt.
+                  </small>
+                </p>
+                <ul>
+                  {TOOL_DEFINITIONS.map((tool) => (
+                    <li key={tool.name}>
+                      <h3>{tool.name}</h3>
+
+                      {tool.paramsSchema ? (
+                        <dl class="params">
+                          {Object.entries(tool.paramsSchema).map(
+                            ([key, value]) => {
+                              return (
+                                <>
+                                  <dt key={key}>
+                                    <code>{key}</code>
+                                  </dt>
+                                  <dd key={key}>{value.description}</dd>
+                                </>
+                              );
+                            },
+                          )}
+                        </dl>
+                      ) : null}
+
+                      <p>{tool.description.split("\n")[0]}</p>
+                    </li>
+                  ))}
+                </ul>
+              </section>
+
+              <section id="resources">
+                <h2>Additional Resources</h2>
+                <ul>
+                  <li>
+                    <a href="https://github.com/getsentry/sentry-mcp">
+                      sentry-mcp on GitHub
+                    </a>
+                  </li>
+                </ul>
+              </section>
+            </article>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#overview">Overview</a>
+                  <ul>
+                    <li>
+                      <a href="#with-cursor">With Cursor</a>
+                    </li>
+                    <li>
+                      <a href="#with-windsurf">With Windsurf</a>
+                    </li>
+                    <li>
+                      <a href="#with-vscode">With VSCode</a>
+                    </li>
+                  </ul>
                 </li>
-              ))}
-            </ul>
-          </section>
-        </article>
+                <li>
+                  <a href="#workflows">Workflows</a>
+                </li>
+                <li>
+                  <a href="#tools">Tools</a>
+                </li>
+                <li>
+                  <a href="#resources">Resources</a>
+                </li>
+              </ul>
+            </nav>
+          </main>
+        </div>
 
-        <h2>Additional Resources</h2>
-        <ul>
-          <li>
-            <a href="https://github.com/getsentry/sentry-mcp">
-              sentry-mcp on GitHub
-            </a>
-          </li>
-        </ul>
         <script dangerouslySetInnerHTML={{ __html: copyPasteHelper }} />
       </body>
     </html>,
