58d7fb77d0c83eb4089e1da8a3871b0fb6d29912
David Cramer <dcramer@gmail.com>
2025-04-12 16:49:44 -0400

    Move evals into separate config

--- src/evals/utils.ts
+++ src/evals/utils.ts
@@ -25,6 +25,7 @@ export function Factuality(model: LanguageModel) {
        */
       prompt: `
         You are comparing a submitted answer to an expert answer on a given question. Here is the data:
+
         [BEGIN DATA]
         ************
         [Question]: ${opts.input}
@@ -35,8 +36,10 @@ export function Factuality(model: LanguageModel) {
         ************
         [END DATA]
 
-        Compare the factual content of the submitted answer with the expert answer. Ignore any differences in style, grammar, or punctuation.
+        Compare the factual content of the submitted answer with the expert answer. Ignore any differences in style, grammar, or punctuation, or overall structure.
+
         The submitted answer may either be a subset or superset of the expert answer, or it may conflict with it. Determine which case applies. Answer the question by selecting one of the following options:
+        
         (A) The submitted answer is a subset of the expert answer and is fully consistent with it.
         (B) The submitted answer is a superset of the expert answer and is fully consistent with it.
         (C) The submitted answer contains all the same details as the expert answer.
